1f03ac68e7debee7b271fb3309c71d49305199f4
jeffersonsavidotti <jeffersonsavidotti@gmail.com>
2023-10-19 00:48:11 -0300

    Correções

--- ProjetoAutomacaoDeFolha/ProjetoFolha/Controllers/ConsultaBeneficioController.cs
+++ ProjetoAutomacaoDeFolha/ProjetoFolha/Controllers/ConsultaBeneficioController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoFolha.Models;
+using ProjetoFolha.Repositorio;
+using System.Diagnostics;
+using System.Text;
+using ProjetoFolha.Helper;
 
 namespace ProjetoFolha.Controllers
 {
     public class ConsultaBeneficioController : Controller
     {
-        public IActionResult Index()
+        private readonly Helper.ISession _session;
+        public ConsultaBeneficioController(ILoginViewRepositorio loginviewrepositorio, Helper.ISession session)
+        {
+
+            _session = session;
+        }
+            public IActionResult Index()
         {
             return View();
         }
+
+
     }
+
+
 }
